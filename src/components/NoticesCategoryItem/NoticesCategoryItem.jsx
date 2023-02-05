679cd138dfca5757893585b4cbfa22c0a1f7e53b
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-05 21:18:34 +0200

    comment NoticesCategoryItem

--- src/components/NoticesCategoryItem/NoticesCategoryItem.jsx
+++ src/components/NoticesCategoryItem/NoticesCategoryItem.jsx
@@ -1,19 +1,17 @@
-
-import Modal from "components/Modal/Modal";
-import { useToggle } from "hooks";
-import { AiFillDelete, AiOutlineHeart } from "react-icons/ai";
+// import Modal from 'components/Modal/Modal';
+// import { useToggle } from 'hooks';
+// import { AiFillDelete } from 'react-icons/ai';
 import {
   AddToFavoriteStyled,
   ButtonStyled,
   ButtonThumbStyled,
   CategoryStyled,
   ImageThumbStyled,
-  NoticesCategoryItemStyled
-} from "./NoticesCategoryItem.styled";
+  NoticesCategoryItemStyled,
+} from './NoticesCategoryItem.styled';
 
-
-const NoticesCategoryItem = ({category}) => {
-const { isOpen, open, close } = useToggle()
+const NoticesCategoryItem = ({ category }) => {
+  // const { isOpen, open, close } = useToggle();
 
   return (
     <NoticesCategoryItemStyled>
@@ -24,27 +22,49 @@ const { isOpen, open, close } = useToggle()
           width={280}
           height={288}
         />
-      </ImageThumbStyled>    
+      </ImageThumbStyled>
 
       <CategoryStyled>{category}</CategoryStyled>
-      <AddToFavoriteStyled isFavorite={false} />  
+      <AddToFavoriteStyled isFavorite={false} />
 
       <h2>Cute cat looking for a home</h2>
       <ul>
-        <li><p style={{marginRight: '37px'}}>Breed:</p><p>Wild cat</p></li>
-        <li><p style={{marginRight: '40px'}}>Place:</p><p>Lviv</p></li>
-        <li><p style={{marginRight: '50px'}}>Age:</p><p>One year</p></li>
+        <li>
+          <p style={{ marginRight: '37px' }}>Breed:</p>
+          <p>Wild cat</p>
+        </li>
+        <li>
+          <p style={{ marginRight: '40px' }}>Place:</p>
+          <p>Lviv</p>
+        </li>
+        <li>
+          <p style={{ marginRight: '50px' }}>Age:</p>
+          <p>One year</p>
+        </li>
       </ul>
-      <ButtonStyled type="button" onClick={open}>Learn more</ButtonStyled>
-      <Modal isOpen={isOpen} onClose={close}><div>Text</div></Modal>
-      <ButtonStyled type="button"><span>Delete</span> <AiFillDelete size={20} color={'#FF6101'} /></ButtonStyled>
+      {/* <ButtonStyled type="button" onClick={open}>
+        Learn more
+      </ButtonStyled>
+      <Modal isOpen={isOpen} onClose={close}>
+        <div>Text</div>
+      </Modal>
+      <ButtonStyled type="button">
+        <span>Delete</span> <AiFillDelete size={20} color={'#FF6101'} />
+      </ButtonStyled> */}
       <ButtonThumbStyled>
-        <ButtonStyled type="button" onClick={() => console.log('v')} style={{backgroundImage: 'none'}}>Learn more</ButtonStyled>
-        <ButtonStyled type="button"><div>Delete</div></ButtonStyled>
+        <ButtonStyled
+          type="button"
+          onClick={() => console.log('v')}
+          style={{ backgroundImage: 'none' }}
+        >
+          Learn more
+        </ButtonStyled>
+        <ButtonStyled type="button">
+          <div>Delete</div>
+        </ButtonStyled>
       </ButtonThumbStyled>
-    </ NoticesCategoryItemStyled>    
+    </NoticesCategoryItemStyled>
   );
-    
 };
 
 export default NoticesCategoryItem;
