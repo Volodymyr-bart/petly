a27d67278b0dbc04dbb34c4f2787075dde681165
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-07 19:44:14 +0200

    delete in newslist

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -5,38 +5,36 @@ import {
   NewsBottom,
   NewsItem,
   NewsItemDescription,
-  NewsItemTitle,
   NewsLink,
   NewsListStyled,
 } from './NewsList.styled';
 
-
-import React from "react";
-import EllipsisText from "react-ellipsis-text";
+import React from 'react';
+import EllipsisText from 'react-ellipsis-text';
 
 //allow react dev tools work
 window.React = React;
 
-
-
-
 const NewsList = () => {
   const news = useSelector(selectAllNews);
 
- 
   return (
     <NewsListStyled>
       {news &&
         news.map(item => (
           <NewsItem key={item.url}>
             <Line />
-            <EllipsisText text={item.title} length={"5"} />
+            <EllipsisText text={item.title} length={'5'} />
             {/* <NewsItemTitle>{item.title}</NewsItemTitle> */}
             <NewsItemDescription>{item.description}</NewsItemDescription>
             <NewsBottom>
               <p>{item.date}</p>
               {/* <a href="{{item.url}}">{item.url}</a> */}
-              <NewsLink href="{item.url}" target="_blank" rel="noreferrer noopener">
+              <NewsLink
+                href="{item.url}"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
                 Read more
               </NewsLink>
             </NewsBottom>
