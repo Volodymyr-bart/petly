140b8eee1c6b38042d6f54ad4066d2a5d32269da
TetianaPiatykh <tanya.pjatykh@gmail.com>
2023-02-09 19:38:13 +0200

    h1

--- src/pages/OurFriends/OurFriend.styled.jsx
+++ src/pages/OurFriends/OurFriend.styled.jsx
@@ -6,7 +6,7 @@ export const ServiceWrapper = styled.div`
 `;
 
 export const ServiceTitle = styled.h1`
-  margin: 42px auto 28px;
+  margin: 53px auto 28px;
   font-family: 'Manrope';
   font-style: normal;
   font-weight: 700;
@@ -15,7 +15,7 @@ export const ServiceTitle = styled.h1`
   text-align: center;
   
   @media (min-width: 768px) {
-    margin: 70px auto 40px;
+    margin: 88px auto 40px;
     font-weight: 700;
     font-size: 48px;
     line-height: 66px;
