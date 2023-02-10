6649f21f6986a79198b5a18f878fee1fa5cff9dd
LiliiaBarsuk <lilya.b@live.com>
2023-02-10 21:49:27 +0200

    fixed schema

--- src/components/ModalAddNotice/ModalAddNoticeValidation.jsx
+++ src/components/ModalAddNotice/ModalAddNoticeValidation.jsx
@@ -25,14 +25,7 @@ export const validationFistStep = yup.object().shape({
     .max(24, 'Too long!')
     .matches(titleRegexp, 'Must contain only letters and spaces')
     .required('Breed is required'),
-  //   birthday: yup.date().nullable(),
-});
-
-const FILE_SIZE = 5242880;
-const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
-
-export const validationSecondStep = yup.object().shape({
-  location: yup
+    location: yup
     .string()
     .trim()
     .matches(locationRegexp, 'For example, "Brovary, Kyiv" or "Dubno, Rivne"')
@@ -55,5 +48,10 @@ export const validationSecondStep = yup.object().shape({
       'FILE_FORMAT',
       'Uploaded file unsupported format',
       value => !value || (value && SUPPORTED_FORMATS.includes(value?.type))
-    ),
+    ), 
+  //   birthday: yup.date().nullable(),
 });
+
+const FILE_SIZE = 5242880;
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+
