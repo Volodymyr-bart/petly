9fc98e47fd22c22b37f7967a6edd6fb4db9b297b
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-12 00:21:46 +0200

    edit price

--- src/components/ModalAddNotice/ModalAddNotice.jsx
+++ src/components/ModalAddNotice/ModalAddNotice.jsx
@@ -1,93 +1,106 @@
 /* eslint-disable no-dupe-keys */
-import { Formik } from 'formik';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  Button,
-  CancelBack,
-  ContainerButton
-} from './ModalAddNotice.styled';
-import { addNotice } from 'redux/notices/operations';
-import { FormContainer } from './ModalAddNotice.styled';
-import { ModalAddNoticeFistStep } from './ModalAddNoticeFirstStep';
-import { ModalAddNoticeSecondStep } from './ModalAddNoticeSecondStep';
-import { validationSchemaNotices } from './ModalAddNoticeValidation';
-const initialValues ={
-  title: '',
-  name: '',
-  birthday: '',
-  breed: '',
-  category: '',
-  theSex: '',
-  location: '',
-  price: '0',
+import { Formik } from "formik";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Button, CancelBack, ContainerButton } from "./ModalAddNotice.styled";
+import { addNotice } from "redux/notices/operations";
+import { FormContainer } from "./ModalAddNotice.styled";
+import { ModalAddNoticeFistStep } from "./ModalAddNoticeFirstStep";
+import { ModalAddNoticeSecondStep } from "./ModalAddNoticeSecondStep";
+import { validationSchemaNotices } from "./ModalAddNoticeValidation";
+const initialValues = {
+  title: "",
+  name: "",
+  birthday: "",
+  breed: "",
+  category: "",
+  theSex: "",
+  location: "",
+  price: "1",
   petAvatar: null,
-  comments: '',
+  comments: "",
   // eslint-disable-next-line no-dupe-keys
-//   category: ''
-}
-
+  //   category: ''
+};
 
 export const ModalAddNotice = ({ onClose }) => {
   const [isLastStep, setisLastStep] = useState(false);
 
   const dispatch = useDispatch();
-  
-  const onhandleSubmit = (values, {resetForm}) => {
+
+  const onhandleSubmit = (values, { resetForm }) => {
     console.log(values);
-    
-    dispatch(addNotice({
-      title: values.title,
-      name: values.name,
-      birthday: values.birthday,
-      category: values.category,
-      breed: values.breed,
-      theSex: values.theSex,
-      location: values.location,
-      price: values.price,
-      petAvatar: values.petAvatar,
-      comments: values.comments,
-    //   category: values.category,
-    }))
-    resetForm()
-    onClose()
+
+    dispatch(
+      addNotice({
+        title: values.title,
+        name: values.name,
+        birthday: values.birthday,
+        category: values.category,
+        breed: values.breed,
+        theSex: values.theSex,
+        location: values.location,
+        price: values.price,
+        petAvatar: values.petAvatar,
+        comments: values.comments,
+        //   category: values.category,
+      })
+    );
+    resetForm();
+    onClose();
   };
-  
+
   return (
- 
-    <Formik 
+    <Formik
       validationSchema={validationSchemaNotices}
       initialValues={initialValues}
-      onSubmit={onhandleSubmit}
-    >
-      {({ values, errors, setFieldValue, dirty, touched, isValid, handleChange}) => { 
-          console.log(errors);
-        return (<FormContainer autoComplete='off'>
-          {isLastStep ? <ModalAddNoticeSecondStep
-          setisLastStep={setisLastStep}
-          onClose={onClose}
-          values={values}
-          // errors={errors}
-          dirty={dirty}
-          handleChange={handleChange}
-          setFieldValue={setFieldValue}
-          /> : <ModalAddNoticeFistStep
-              values={values}
-              errors={errors}
-          />}
+      onSubmit={onhandleSubmit}>
+      {({ values, errors, setFieldValue, dirty, touched, isValid, handleChange }) => {
+        console.log(errors);
+        return (
+          <FormContainer autoComplete="off">
+            {isLastStep ? (
+              <ModalAddNoticeSecondStep
+                setisLastStep={setisLastStep}
+                onClose={onClose}
+                values={values}
+                // errors={errors}
+                dirty={dirty}
+                handleChange={handleChange}
+                setFieldValue={setFieldValue}
+              />
+            ) : (
+              <ModalAddNoticeFistStep values={values} errors={errors} />
+            )}
 
-          <ContainerButton>
-          {isLastStep ? ( <><CancelBack type="button" onClick={() => setisLastStep(false)}>Back</CancelBack>
-                {isValid ? (<Button type="submit">Done</Button>) : (<Button disabled={true} className="disabled" >Done</Button>)}</>) : (<><CancelBack type="button" onClick={onClose}>
-              Cancel
-            </CancelBack>
-                <Button type="button" onClick={() => setisLastStep(true)}>
-                  Next
-                </Button>
-              </>)}
-            
-          </ContainerButton>
-      </FormContainer>)}}
-
-    </Formik>)
+            <ContainerButton>
+              {isLastStep ? (
+                <>
+                  <CancelBack type="button" onClick={() => setisLastStep(false)}>
+                    Back
+                  </CancelBack>
+                  {isValid ? (
+                    <Button type="submit">Done</Button>
+                  ) : (
+                    <Button disabled={true} className="disabled">
+                      Done
+                    </Button>
+                  )}
+                </>
+              ) : (
+                <>
+                  <CancelBack type="button" onClick={onClose}>
+                    Cancel
+                  </CancelBack>
+                  <Button type="button" onClick={() => setisLastStep(true)}>
+                    Next
+                  </Button>
+                </>
+              )}
+            </ContainerButton>
+          </FormContainer>
+        );
+      }}
+    </Formik>
+  );
 };
