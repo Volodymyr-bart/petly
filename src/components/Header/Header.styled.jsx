a68afc1ee7647a4c5d12fda28e1fe5bdd40577b8
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2023-02-17 20:30:46 +0200

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -4,13 +4,15 @@ export const HeaderStyled = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  max-width: 1280px;
+  /* max-width: 1280px; */
   margin-left: auto;
   margin-right: auto;
   padding-left: ${p => p.isHome ? '20px' : '0'};
   padding-right: ${p => p.isHome ? '20px' : '0'};
+  width: ${p => p.isHome && '320px'};
 
-  @media (min-width: 768px) {    
+  @media (min-width: 768px) {
+    width: ${p => p.isHome && '768px'};  
     padding-left: ${p => p.isHome ? '32px' : '0'};
     padding-right: ${p => p.isHome ? '32px' : '0'};
   }
@@ -18,6 +20,7 @@ export const HeaderStyled = styled.header`
 
   @media (min-width: 1280px) {
     align-items: baseline;
+    width: ${p => p.isHome && '1280px'};
     padding-left: ${p => p.isHome ? '16px' : '0'};
     padding-right: ${p => p.isHome ? '16px' : '0'};
   }
